c20ff652b4611a4b16638b703abc41d08ef886b1
Студент НИУ ВШЭ <student@basm-402-std22.staff.corp.local>
2017-09-19 11:53:28 +0300

    Материалы первого семинара

---
+++ first_class.py
@@ -0,0 +1,22 @@
+print('Hello!')
+
+#x = 10
+#y = x*100   # умножаю на сто
+
+#print ('y =', y)
+#print ('x =', x, ', y =', y)
+
+#human_name = input ('Как вас зовут? ')
+#print ('Привет,', human_name)
+#print (type (human_name))
+
+number = int (input (' Введите число '))
+#x2 = int (input ('Введите второе число '))
+#num1 = int (x1)
+#num2 = int (x2)
+#print (type (x1), type (x2))
+#print (x1*x2)
+
+if number > 5:
+    print ('Ваше число больше пяти')
+    print ('!!')
