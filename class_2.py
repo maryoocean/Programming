c1de5cb09100fadcd867df2d00818b9d8a054e85
Студент НИУ ВШЭ <student@basm-402-std22.staff.corp.local>
2017-09-26 11:46:04 +0300

    Конспект второго занятия

---
+++ class_2.py
@@ -0,0 +1,103 @@
+#num = int (input ('Введите число '))
+#print (num)
+
+#word_length = len (word)
+#print (word_length)
+
+#if word_length >= 5:
+    #print ('Looong')
+    #print ('Long!')
+#print ('End.')
+
+#if num > 0:
+#    print ('num =' , num)
+#   print ('num*10 =' , num*10)
+#else:
+#    num = -num
+#print (num)
+#print ('End.')
+
+#if num == 0:
+#    print ('num =' , num)
+  
+#else:
+#    num = num + 100
+#print (num)
+#print ('End.')
+
+#if num != 0:
+#    print ('num =' , num)
+  
+#else:
+#    #num = num + 100
+#    num += 100
+#print (num)
+#print ('End.')
+
+#if num > 0:
+#    print (num)
+#    print ('Больше нуля')
+#else:
+#    print ('Не больше нуля')
+#   if num > -2:
+#        print ('Больше -2')
+#    else:
+#        print ('Не больше -2')
+#print (num)
+#print ('End.')
+
+#if num > 0 and num <=5:
+#    print (num, 'строго больше 0 и меньше или равно 5')
+#print ('End.')
+
+#if num > 0 or num <=5:
+    #print (num, 'строго больше 0 или меньше или равно 5')
+#print ('End.')
+
+#if not 2 < 3:
+#    print ('hello')
+#else:
+#    print ('Nooooo')
+#print ('End.')
+
+#x = int (input ('Введите первое число'))
+#y = int (input ('Введите второе число'))
+#if x % 2 == 0 and y % 2 != 0:
+#    print ('Оба четные')
+#else x % 2 == 0 and y % 2 != 0:
+#    print ('x, но не y)
+#else x % 2 != 0 and y% 2 == 0:
+#    print ('y, но не x)
+
+    #num1 = int (input ('Введите первое число'))
+#num2 = int (input ('Введите второе число'))
+#if num1 % 2 == 0 and num2 % 2 == 0:
+#    print ('Оба числа четные')
+#else:
+#    print ('num1 или num2 нечетное')
+
+#x = int (input ("Введите первое число "))
+#if x > 0:
+#    print(1)
+#elif x == 0:
+#    print (0)
+#else:
+#    print (-1)
+
+#year = int ((input ('Введите год'))
+#if year % 4 == 0 and not year % 100 == 0 or year % 400 == 0:
+#    print ('Год високосный')
+#else:
+#    print ('Год невисокосный')
+
+year = int ((input ('Введите год'))
+if year % 4 == 0 and not year % 100 == 0 or year % 400 == 0:
+    print ('Год високосный')
+else:
+    print ('Год невисокосный')
+
+
+
+
+
+
