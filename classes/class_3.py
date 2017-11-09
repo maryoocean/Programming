6eff425496048b350b82bfc3d3c350d40808c824
Студент НИУ ВШЭ <student@basm-402-std06.staff.corp.local>
2017-11-09 12:04:44 +0300

    Семинар 09.11

---
+++ classes/class_3.py
@@ -0,0 +1,73 @@
+#Списки (list)
+#people = ['anna', 'john','kate']
+#name = 'kate'
+#if name in people:
+#   print('yes')
+#nums, apend
+#([0, 1, 2, 3], [100, 200, 300])
+#nums = [0,1,2,3,6,7]
+#nums, apend
+#([0, 1, 2, 3, 6, 7], [100, 200, 300])
+#nums [-1]
+#7
+#nums = nums, apend
+#nums [-1]
+#[100, 200, 300]
+
+#nums = [1,2,3]
+#for n in nums:
+#    print(n)
+
+#Срез списка
+#text = 'food sleep walking'
+#words = text.split()
+#print(words)
+#print(words[0:2])
+
+#text = 'food sleep walking'
+#words = text.split()
+#print(words)
+#print(words[::2])
+
+#nums = [1,2,3]*3
+#print(nums)
+#for n in row:
+#    print (n
+
+#Разделить символы
+
+#w = 'abcd'
+
+
+#nums = [1,2,3]
+#nums_str = str(nums[0])
+#for n in nums:
+#    nums_str = nums_str + str(1) + ''    
+#print(nums_str)
+
+#операция, противоположная сплиту - джоин
+
+#letters = list('hello')
+#letters.pop(-2)
+#print(letters)
+
+#count - считает, сколько раз элемент встретился; index
+
+#nums = [2,3,14,1,1,2]
+#print(sorted(nums))
+
+#Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+#Type "copyright", "credits" or "license()" for more information.
+#>>> letters = list('hello')
+#>>> letters
+#['h', 'e', 'l', 'l', 'o']
+#>>> new = letters.pop()
+#>>> letters
+#['h', 'e', 'l', 'l']
+#>>> new
+#'o'
+
+nums = [[1,2,3,4]*5]
+for row in nums:
+    
+
